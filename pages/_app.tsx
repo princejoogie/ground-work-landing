ca2b3e9cab92116112aa6732647de6764d064ca2
princejoogie <princejoogie@gmail.com>
2021-10-03 03:41:26 +0800

    ➕ added  page loading bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import "../styles/globals.css";
+import "nprogress/nprogress.css";
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
