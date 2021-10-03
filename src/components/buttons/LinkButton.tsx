a8f4102b510fcf1b64da92fde0d177108bcf4747
Zomer <zomerzxc.19@gmail.com>
2021-10-04 05:46:27 +0800

    Add LinkButton

---
+++ src/components/buttons/LinkButton.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Link from 'next/link';
+
+type TargetType = '_self' | '_blank' | '_parent' | '_top';
+
+type RelType = 'noreferrer' | 'noopener' | 'nofollow' | 'opener';
+
+interface LinkButtonProps {
+  href: string;
+  text: string;
+  target?: TargetType;
+  rel?: RelType;
+}
+
+const LinkButton: React.FC<LinkButtonProps> = ({ href, text, target, rel }) => {
+  return (
+    <Link href={href}>
+      <a
+        className='px-6 py-2 text-xs font-semibold uppercase transition-colors duration-300 border-2 border-black rounded-full lg:text-base hover:text-white hover:bg-black'
+        target={target}
+        rel={rel}
+      >
+        {text}
+      </a>
+    </Link>
+  );
+};
+
+export default LinkButton;
