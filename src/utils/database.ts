1e9ed65dd68ff1e078661539c9254325890a37c7
princejoogie <princejoogie@gmail.com>
2021-10-03 18:18:09 +0800

    ✏ configured databse connection

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -4,7 +4,13 @@ import "firebase/auth";
 import "firebase/storage";
 
 const firebaseConfig = {
-  // TODO: #11 fill in with config
+  apiKey: "AIzaSyBBHrU13VwTxttMxPl8siBAfkAMd5NpB9o",
+  authDomain: "ground-work-ph.firebaseapp.com",
+  projectId: "ground-work-ph",
+  storageBucket: "ground-work-ph.appspot.com",
+  messagingSenderId: "222261590833",
+  appId: "1:222261590833:web:b601647a7079b024160e10",
+  measurementId: "G-SSPPQ6J5MZ",
 };
 
 if (!firebase.apps.length) {
@@ -17,6 +23,15 @@ const db = firebase.firestore();
 const auth = firebase.auth();
 const storage = firebase.storage();
 const GoogleProvider = new firebase.auth.GoogleAuthProvider();
-const GithubProvider = new firebase.auth.GithubAuthProvider();
+const FacebookProvider = new firebase.auth.FacebookAuthProvider();
+const YahooProvider = new firebase.auth.OAuthProvider("yahoo.com");
 
-export { firebase, db, auth, storage, GoogleProvider, GithubProvider };
+export {
+  FacebookProvider,
+  GoogleProvider,
+  YahooProvider,
+  auth,
+  db,
+  firebase,
+  storage,
+};
