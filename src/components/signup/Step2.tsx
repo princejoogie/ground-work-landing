b743f44b0d7c5d6dd4afa4b9beb2faa268c5dc87
Josh Daniel <joshpersonal8@gmail.com>
2021-10-04 20:58:19 +0800

    feat: add media queries

--- src/components/signup/Step2.tsx
+++ src/components/signup/Step2.tsx
@@ -11,18 +11,18 @@ const Step2 = ({ setStep }: Step2Props) => {
   return (
     <form spellCheck="false" className="signup-form pb-16">
       <div className="w-full flex flex-col items-center py-8 shadow-lg rounded-b-lg">
-        <h1 className="font-bold text-3xl text-secondary mb-8">
+        <h1 className="font-bold text-2xl md:text-3xl  text-secondary mb-8">
           Fill in Your Basic Info
         </h1>
 
-        <div className="flex justify-between w-[70%]">
-          <CurrentStepCircle num="1" desc="Type" />
+        <div className="flex justify-between w-[90%] sm:w-[70%]">
+          <CurrentStepCircle num="1" desc="Sign Up" />
           <CurrentStepCircle num="2" desc="Basic Info" />
           <StepCircle num="3" desc="More Info" />
         </div>
       </div>
 
-      <div className="w-[70%] mt-8 flex flex-col">
+      <div className="w-[90%] sm:w-[70%] mt-8 flex flex-col">
         <Input label="First Name" />
         <Input label="Last Name" />
         <Select choices={["Male", "Female", "Others"]} label="Gender" />
--- src/components/signup/Step3.tsx
+++ src/components/signup/Step3.tsx
@@ -12,18 +12,18 @@ const Step3 = ({ setStep }: Step1Props) => {
   return (
     <form spellCheck="false" className="signup-form">
       <div className="w-full flex flex-col items-center py-8 shadow-lg rounded-b-lg">
-        <h1 className="font-bold text-3xl text-secondary mb-8">
+        <h1 className="font-bold text-2xl md:text-3xl text-secondary mb-8">
           Almost There!
         </h1>
 
-        <div className="flex justify-between w-[70%]">
-          <CurrentStepCircle num="1" desc="Type" />
+        <div className="flex justify-between w-[90%] sm:w-[70%]">
+          <CurrentStepCircle num="1" desc="Sign Up" />
           <CurrentStepCircle num="2" desc="Basic Info" />
           <CurrentStepCircle num="3" desc="More Info" />
         </div>
       </div>
 
-      <div className="w-[70%] mt-8 flex flex-col">
+      <div className="w-[90%] sm:w-[70%] mt-8 flex flex-col">
         <Input label="Company Name" />
         <Select
           label="Business Industry"
